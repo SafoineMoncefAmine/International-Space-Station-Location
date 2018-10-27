ebc2582dedcbf03d78910d44187686036068d967
Safoine Moncef Amine <safoinemoncefamine@gmail.com>
2018-10-27 19:36:40 +0100

    check if ISS is near

--- Avito Challenge/Avito Challenge/views/controllers/MapViewController.swift
+++ Avito Challenge/Avito Challenge/views/controllers/MapViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController  {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var isNearButton: UIButton!
+    var locationManager = CLLocationManager()
     let regionRadius: CLLocationDistance = 500000
+    let issNearRegionRadius: CLLocationDistance = 10000
     let annotation = MKPointAnnotation()
 
     var issLocation: CLLocation {
         let latitude = Double(self.latitude)!
         let longitude = Double(self.longitude)!
-        
-        print(latitude , longitude)
         return CLLocation(latitude: Double(self.latitude)!, longitude: Double(self.longitude)!)
     }
     var latitude : String = ""
@@ -29,9 +31,25 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var _ = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(MapViewController.updateIssLocation), userInfo: nil, repeats: true)
-        
+        configure()
     }
-
+    
+    func configure() {
+        locationManager.delegate = self
+    }
+    
+    func checkIfIssIsNear() -> Bool {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        if locationManager.location!.distance(from: issLocation) > issNearRegionRadius {
+            self.isNearButton.backgroundColor = UIColor.red
+        }else {
+            self.isNearButton.backgroundColor = UIColor.green
+        }
+        return true
+    }
+    
     @objc private func updateIssLocation(){
         ISSApi.shared.getCurrentISSLocation { (latitude, longitude) -> Void in
             self.latitude = latitude
@@ -40,6 +58,7 @@ class MapViewController: UIViewController {
             self.mapView.removeAnnotations(self.mapView.annotations)
             self.mapView.addAnnotation(self.annotation)
         }
+        self.checkIfIssIsNear()
     }
     
 
@@ -51,5 +70,10 @@ class MapViewController: UIViewController {
     @IBAction func showIssPassengers(_ sender: Any) {
         performSegue(withIdentifier: "showPassengers", sender: self)
     }
+    
+
 }
 
+extension MapViewController : CLLocationManagerDelegate {
+    
+}
